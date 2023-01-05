cbedf8ff2da8d31c2ab109bb0e2b6cca5ac8d0dc
yongbeom76 <yongbeom76@gmail.com>
2023-01-05 19:31:34 +0900

    board

--- src/main/java/com/study/study_springboots/controller/BoardController.java
+++ src/main/java/com/study/study_springboots/controller/BoardController.java
@@ -11,9 +11,25 @@ public class BoardController {
         return "/WEB-INF/views/board/form.jsp";
     }
 
+    // form tag통해서 content를 작성해서 날린 경우 post로 날라온다. 그 데이타를 받아와라.
     @RequestMapping(value = "/board/form", method = RequestMethod.POST)
     public String formPost() {
         // insert biz
         return "/WEB-INF/views/board/list.jsp";
+    }
+
+    @RequestMapping(value = { "/board", "/board/list" })
+    public String board() {
+        return "/WEB-INF/views/board/list.jsp";
+    }
+
+    @RequestMapping(value = "/board/view")
+    public String view() {
+        return "/WEB-INF/views/board/view.jsp";
+    }
+
+    @RequestMapping(value = "/board/edit")
+    public String edit() {
+        return "/WEB-INF/views/board/edit.jsp";
     }
 }
